4954e176347b9a911e5c4b6bb7262a04fd8a46c3
Afif Sohaili <afif@endpoint.com>
2021-06-09 02:44:50 +0800

    Add simple Vue example on main layout.

---
+++ app/javascript/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <p>{{ message }} - {{ count }}</p>
+    <button @click="count++">Click meh</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello Vue!",
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
