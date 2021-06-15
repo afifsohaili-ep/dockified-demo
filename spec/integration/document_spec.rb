7e8ae103212a74e038dbd188e2b1499baa5f3fdc
Afif Sohaili <afif@endpoint.com>
2021-06-15 16:43:35 +0800

    Add a new spec, update appearance of button on /documents/new page.

--- spec/integration/document_spec.rb
+++ spec/integration/document_spec.rb
@@ -4,9 +4,7 @@ require "percy"
 RSpec.feature "Add documents", js: true do
 	before :each do
 		User.create(email: 'user@example.com', password: 'password')
-	end
 
-	scenario "User creates a new document" do
 		visit "/documents/new"
 
 		within("#new_user") do
@@ -15,8 +13,19 @@ RSpec.feature "Add documents", js: true do
 		end
 
 		click_button 'Log in'
+	end
 
+	scenario "User visits page to create new document" do
 		expect(page).to have_text('Create new document')
 		Percy.snapshot(page, { name: 'Create document page' })
+	end
+
+	scenario "User sees validation errors" do
+		click_button 'Create'
+
+		expect(page).to have_text('Title can\'t be blank')
+		expect(page).to have_text('Body can\'t be blank')
+		expect(page).to have_text('Body is too short (minimum is 10 characters)')
+		Percy.snapshot(page, { name: 'Create document page validation error' })
 	end
 end
